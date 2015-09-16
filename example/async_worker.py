e55ced012d82fd97db3142d9b516e8669622c36c
Oleg Nechaev <lega911@gmail.com>
2015-09-16 16:45:11 +0500

    async worker

---
+++ example/async_worker.py
@@ -0,0 +1,26 @@
+
+import asyncio
+import jclient
+
+
+@asyncio.coroutine
+def ping(raw):
+    yield from asyncio.sleep(1)
+    return b'pong'
+
+
+def echo(raw):
+    return raw
+
+
+@asyncio.coroutine
+def worker(loop):
+    rpc = jclient.WorkerAsyncHandler('localhost', 8011, loop=loop)
+    rpc.add('ping', ping)
+    rpc.add('echo', echo)
+    yield from rpc.serve()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(worker(loop))
+loop.close()
